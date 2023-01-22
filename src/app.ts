51aed544013be7cdbd0fbfbe8192edf525a5ed0b
Russell Payne <ryois@ryois.me>
2023-01-22 02:10:09 -0500

    WS

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
+import WebSocket from 'ws';
+import http from 'http';
 import express from 'express';
+
 const app = express();
 const port = 3000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+wss.on('connection', (ws: WebSocket) => {
+
+  //connection is up, let's add a simple simple event
+  ws.on('message', (message: string) => {
+
+      //log the received message and send it back to the client
+      console.log('received: %s', message);
+      ws.send(`Hello, you sent -> ${message}`);
+  });
+
+  //send immediatly a feedback to the incoming connection    
+  ws.send('CONNECTED');
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+//start our server
+server.listen(process.env.PORT || port, () => {
+  console.log(`Server started on port ${port}`);
 });
